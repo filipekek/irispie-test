8c356b4117a773e26c7daa0a45697c363e6ebc54
Filip Benes <filipb.may@gmail.com>
2023-02-12 17:43:27 +0100

    All OK

--- test_parser2.py
+++ test_parser2.py
@@ -11,11 +11,12 @@ source = r"""
 
 
     !transition-shocks
-        "Shocks x1-x4" shk_x1 shk_x2, 
+        "Shock x1" shk_x1
+        "Shock x2" shk_x2,
         shk_x3; shk_x4
 
     !transition-variables
-        "Variable x1" x1 
+        "Variable x1" x1 "Variable x3" x3
 
     !parameters
         rho_x1 ss_x1
@@ -24,11 +25,16 @@ source = r"""
         "Equation x1" x1 = rho_x1 * x1{-1} + (1 - rho_x1) * ss_x1 + shk_x1 !! x1 = 0;
 
     !transition-equations
-        "Equation x2" x2 = shk_x1 * x2 + (3 + ss_x1)
+        "Equation x2" x2 = shk_x1 * x2 + (3 + ss_x1);
 
     !transition-variables
         "Variable x2" x2
 
+    !measurement-variables
+        "M Variable mx1" mx1
+
+    !measurement-shocks
+        m_shk_x1
 
 """
 
@@ -70,61 +76,72 @@ def _assertion(x, manual_x):
 
 def test_quantities_human(m):
     x = _create_list(m, "human")
-    manual_x = ['x1', 'shk_x1', 'rho_x1', 'ss_x1']
+    manual_x = ['x1', 'x3', 'x2', 'shk_x1', 'shk_x2', 'shk_x3', 'shk_x4','mx1', 'm_shk_x1', 'rho_x1', 'ss_x1']
     _assertion(x, manual_x)
 
 def test_quantities_descriptions(m):
     x = _create_list(m, "descript")
-    manual_x = ['Variable x1', '','','']
+    manual_x = ['Variable x1', 'Variable x3','Variable x2','Shock x1', 'Shock x2', '', '','M Variable mx1', '', '', '']
     _assertion(x, manual_x)
 
 def test_quantities_kinds(m, q):
     x = _create_list(m, "kind")
-    manual_x = [q.TRANSITION_VARIABLE, q.TRANSITION_SHOCK, q.PARAMETER, q.PARAMETER]
+    manual_x = [
+    q.TRANSITION_VARIABLE,
+    q.TRANSITION_VARIABLE,
+    q.TRANSITION_VARIABLE,
+    q.TRANSITION_SHOCK,
+    q.TRANSITION_SHOCK,
+    q.TRANSITION_SHOCK,
+    q.TRANSITION_SHOCK,
+    q.MEASUREMENT_VARIABLE,
+    q.MEASUREMENT_SHOCK,
+    q.PARAMETER,
+    q.PARAMETER]
     _assertion(x, manual_x)
 
 def test_quantities_entry(m):
     x = _create_list(m, "entry")
-    manual_x = [1, 0, 2, 3]
+    manual_x = [4, 5, 8, 0, 1, 2, 3, 9, 10, 6, 7,]
     _assertion(x, manual_x)
 
 def test_dynamic_equations_human(m):
     x = _create_list(m, "human")
-    manual_x = ['x1 = rho_x1 * x1{-1} + (1 - rho_x1) * ss_x1 + shk_x1']
+    manual_x = ['x1 = rho_x1 * x1{-1} + (1 - rho_x1) * ss_x1 + shk_x1', 'x2 = shk_x1 * x2 + (3 + ss_x1)']
     _assertion(x, manual_x)
 
 def test_dynamic_equations_descriptions(m):
     x = _create_list(m, "descript")
-    manual_x = ['Equation x1']
+    manual_x = ['Equation x1', 'Equation x2']
     _assertion(x, manual_x)
 
 def test_dynamic_equations_kinds(m, q):
     x = _create_list(m, "kind")
-    manual_x = [q.TRANSITION_EQUATION]
+    manual_x = [q.TRANSITION_EQUATION, q.TRANSITION_EQUATION]
     _assertion(x, manual_x)
 
 def test_dynamic_equations_entry(m):
     x = _create_list(m, "entry")
-    manual_x = [0]
+    manual_x = [0, 1]
     _assertion(x, manual_x)
 
 def test_steady_equations_human(m):
     x = _create_list(m, "human")
-    manual_x = ['x1 = 0']
+    manual_x = ['x1 = 0', 'x2 = shk_x1 * x2 + (3 + ss_x1)']
     _assertion(x, manual_x)
 
 def test_steady_equations_descriptions(m):
     x = _create_list(m, "descript")
-    manual_x = ['Equation x1']
+    manual_x = ['Equation x1', 'Equation x2']
     _assertion(x, manual_x)
 
 def test_steady_equations_kinds(m, q):
     x = _create_list(m, "kind")
-    manual_x = [q.TRANSITION_EQUATION]
+    manual_x = [q.TRANSITION_EQUATION, q.TRANSITION_EQUATION]
     _assertion(x, manual_x)
 
 def test_steady_equations_entry(m):
     x = _create_list(m, "entry")
-    manual_x = [0]
+    manual_x = [0, 1]
     _assertion(x, manual_x)
 
